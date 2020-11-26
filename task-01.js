5c63bee309d46d60340e5faa3a465aaab7848181
Andrii Dorohov <70909934+AndriiDorohov@users.noreply.github.com>
2020-11-26 21:58:01 +0200

    Delete task-01.js

--- task-01.js
+++
@@ -1,7 +0,0 @@
-const name = 'Генератор защитного поля';
-
-let price = 1000;
-const message = 'Выбран name цена за штуку ${price} кредитов';
-console.log(message);
-price = 2000;
-console.log('Выбран ${name} цена за штуку ${price} кредитов');
