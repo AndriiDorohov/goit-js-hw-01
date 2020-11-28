e5baf3a8ddd6e01aa73921e244aacad4507ebeea
Andrii Dorohov <70909934+AndriiDorohov@users.noreply.github.com>
2020-11-28 20:58:34 +0200

    Update task-05.js

--- task-05.js
+++ task-05.js
@@ -6,7 +6,7 @@ const jamaica = 120;
 
 
 let countrySelect = prompt('Введите страну').toLowerCase();                     //преобразовуем в строчные символы введенное слово
-countrySelect = countrySelect.charAt(0).toUpperCase()+countrySelect.substr(1);  //делаем первый символ названия с большой буквы
+countrySelect = countrySelect.slice(0,1).toUpperCase()+countrySelect.slice(1);  //делаем первый символ названия с большой буквы
 
 switch (countrySelect) {
     case 'Китай':
