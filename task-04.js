dfa0f06b8c7e26714d44e080f09c45bf33947da9
Andrii Dorohov <70909934+AndriiDorohov@users.noreply.github.com>
2020-11-28 21:09:11 +0200

    Update task-04.js

--- task-04.js
+++ task-04.js
@@ -3,9 +3,10 @@ let totalPrice = 0;
 const priceDroid = 3000;
 
 let quantity = prompt('Введите количество едениц');
+
 if (quantity === null) {console.log('Отменено пользователем!')}
-  else {totalPrice = (Number(quantity) * priceDroid)};
-    if (totalPrice > credits) {console.log('Недостаточно средств на счету')}
-       else console.log(`Вы купили ${quantity} дронов, на счету осталось ${credits-totalPrice} кредитов`);
+  else {totalPrice = (Number(quantity) * priceDroid);
+     if (totalPrice > credits) {console.log('Недостаточно средств на счету')}
+     else console.log(`Вы купили ${quantity} дронов, на счету осталось ${credits-totalPrice} кредитов`);};
 
 
