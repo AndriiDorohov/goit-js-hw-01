0687f9852226143cec2d14cf8d2b491a3ec517c6
Andrii Dorohov <70909934+AndriiDorohov@users.noreply.github.com>
2020-11-28 20:46:18 +0200

    Update task-03.js

--- task-03.js
+++ task-03.js
@@ -1,9 +1,9 @@
 const ADMIN_PASSWORD = 'jqueryismyjam'
 let message;
-message = prompt('Введите пароль администратора');
+let inputPassword = prompt('Введите пароль администратора');
 
-if (message === null) {message = 'Отменено пользователем!';}
+if (inputPassword === null) {message = 'Отменено пользователем!';}
     else 
-    if (message === ADMIN_PASSWORD) {message = 'Добро пожаловать!';}
+    if (inputPassword === ADMIN_PASSWORD) {message = 'Добро пожаловать!';}
         else {message = 'Доступ запрещен, неверній пароль!'};
 alert(message);
